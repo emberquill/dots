c8018d9001a2895455491c30765942a7c8eef48c
emberquill <rycr2002@gmail.com>
2022-02-22 15:05:06 -0500

    nvim: Possibly fix packer clone issue

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,7 +13,7 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
         'clone',
         '--depth',
         '1',
-        'git@github.com:wbthomason/packer.nvim',
+        'https://github.com/wbthomason/packer.nvim',
         install_path
     })
 end
