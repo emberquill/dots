2e43109ce2f9d50e39a511cc890e4fd93acc9609
emberquill <rycr2002@gmail.com>
2022-02-22 14:30:16 -0500

    nvim: modular init.lua

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+vim.o.syntax = 'on'
+vim.o.number = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.encoding = 'utf-8'
+vim.o.mouse = 'a'
+
+if os.getenv('VIRTUAL_ENV') ~= nil then
+    vim.g.python3_host_prog=vim.fn.substitute(vim.fn.system('which -a python3 | head -n2 | tail -n1'), '\n', '', 'g')
+else
+    vim.g.python3_host_prog=vim.fn.substitute(vim.fn.system('which python3'), '\n', '', 'g')
+end
