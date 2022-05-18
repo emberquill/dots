d91a1899e112ed2690bcca7f31aed513077643e2
emberquill <rycr2002@gmail.com>
2022-05-18 12:36:06 -0400

    Added Darwin (macOS) plugin for Mac-specific config

---
+++ .zshplugins/local/darwin.plugin.zsh
@@ -0,0 +1,33 @@
+#!/usr/bin/env zsh
+
+[[ "$(uname -s)" == "Darwin" ]] || exit 0
+
+unsetopt beep
+
+# Update Homebrew and Dotfiles
+function update() {
+    echo "$(tput bold)==> $(tput setaf 4)Updating Homebrew...$(tput sgr0)"
+    brew update
+    brew upgrade
+    brew cleanup --prune=7
+    echo "$(tput bold)==> $(tput setaf 4)Updating Dotfiles...$(tput sgr0)"
+    dotupdate
+}
+
+# ------------
+# iTerm2 Fixes
+# ------------
+
+# Fix delete key
+bindkey "^[[3~" delete-char
+
+# Word navigation
+bindkey "[D" backward-word
+bindkey "[C" forward-word
+
+# Fix text encoding
+export LC_ALL="en_US.UTF-8"
+
+# Fix clear/reset commands
+alias reset="reset && printf '\33c\e[3J'"
+alias clear="clear && printf '\33c\e[3J'"
