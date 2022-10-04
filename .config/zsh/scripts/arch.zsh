f703a4bcc3023ddbfbf927d597370bf2a380bc48
emberquill <rycr2002@gmail.com>
2022-10-03 21:42:37 -0400

    Added aur/aurto aliases

--- .config/zsh/scripts/arch.zsh
+++ .config/zsh/scripts/arch.zsh
@@ -1,3 +1,8 @@
+#!/usr/bin/env zsh
+
 if (( $+commands[rankmirrors] )); then
     alias mirrors="curl -s 'https://archlinux.org/mirrorlist/?country=US&country=CA&protocol=https&use_mirror_status=on' | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 20 - | sudo tee /etc/pacman.d/mirrorlist"
 fi
+
+alias aurto="ssh -t aurto aurto"
+alias aur="ssh -t aurto aur"
