e7ec590bef460e60023f5e41f1f15120634065c7
emberquill <rycr2002@gmail.com>
2022-12-13 12:34:40 -0500

    Arch: Updated mirrors command to take a country input

--- .config/zsh/scripts/arch.zsh
+++ .config/zsh/scripts/arch.zsh
@@ -1,7 +1,15 @@
 #!/usr/bin/env zsh
 
 if (( $+commands[rankmirrors] )); then
-    alias mirrors="curl -s 'https://archlinux.org/mirrorlist/?country=US&country=CA&protocol=https&use_mirror_status=on' | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 20 - | sudo tee /etc/pacman.d/mirrorlist"
+    function mirrors {
+        local country
+        if (( $# == 0 )); then
+            country="US"
+        else
+            country=$1
+        fi
+        curl -s "https://archlinux.org/mirrorlist/?country=$country&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 20 - | sudo tee /etc/pacman.d/mirrorlist
+    }
 fi
 
 alias aurto="ssh -t aurto aurto"
